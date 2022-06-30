ba7f504a2e1a86e590b73d76134d420a59c21476
carlosWarren <carlos.silva@warren.com.br>
2022-06-30 20:12:59 -0300

    Initial Commit

---
+++ Warren/WarrenApp.swift
@@ -0,0 +1,17 @@
+//
+//  WarrenApp.swift
+//  Warren
+//
+//  Created by Carlos Silva on 30/06/22.
+//
+
+import SwiftUI
+
+@main
+struct WarrenApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
